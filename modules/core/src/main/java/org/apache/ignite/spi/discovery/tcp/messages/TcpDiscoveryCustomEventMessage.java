16429f84118339064be543f43c5fb2ca91624a64
sevdokimov <sevdokimov@gridgain.com>
2015-04-29 12:17:21 +0300

    # IGNITE-831 TcpDiscoveryCustomEventMessage should be sent to client.

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryCustomEventMessage.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryCustomEventMessage.java
@@ -24,6 +24,7 @@ import java.util.*;
 /**
  * Wrapped for custom message.
  */
+@TcpDiscoveryRedirectToClient
 @TcpDiscoveryEnsureDelivery
 public class TcpDiscoveryCustomEventMessage extends TcpDiscoveryAbstractMessage {
     /** */
